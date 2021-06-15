0d8e9cc4dd6598af48f08a7e94cc4fd55fef1d3b
Ace-Krypton <ramizna@code.edu.az>
2021-06-15 17:14:47 +0400

    commit1

---
+++ PharmacyApplication/TypeDrug.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication
+{
+    class TypeDrug
+    {
+        //Properties
+        public string TypeName { get; set; }
+
+        //Fields
+        private static int _id; //For unique ID
+        public readonly int ID; //For unique ID
+
+        //Constructor for unique ID
+        public TypeDrug()
+        {
+            _id++;
+            ID = _id;
+        }
+
+        public override string ToString()
+        {
+            return $"TypeName:{TypeName}";
+        }
+
+    }
+}
